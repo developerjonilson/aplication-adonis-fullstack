03d1ae2fcc185caed505d07d1cf5924254c407af
jonilsondeveloper <jonilsondeveloper@gmail.com>
2019-05-10 14:48:55 -0300

    consultar no banco

--- app/Controllers/Http/LoginController.js
+++ app/Controllers/Http/LoginController.js
@@ -12,24 +12,28 @@ class LoginController {
       const fbUser = await ally.driver('facebook').getUser()
 
       // user details to be saved
-      const userDetails = {
-        email: fbUser.getEmail(),
-        token: fbUser.getAccessToken(),
-        login_source: 'facebook'
-      }
+      // const userDetails = {
+      //   email: fbUser.getEmail(),
+      //   token: fbUser.getAccessToken(),
+      //   login_source: 'facebook'
+      // }
 
       // search for existing user
       const whereClause = {
         email: fbUser.getEmail()
       }
 
-      const userDb = await User.find(whereClause)
+      const user = await User.find(whereClause)
+      // const user = await User.findOrCreate(
+      //   { email: whereClause },
+      //   { username: 'virk', email: 'virk@adonisjs.com' }
+      // )
 
       // const user = await User.findOrCreate(whereClause, userDetails)
       // await auth.login(user)
 
       // return 'Logado'
-      return response.send(userDetails, userDb)
+      return response.send(user)
     } catch (error) {
       return 'Incapaz de autenticar. Tente mais tarde'
     }
