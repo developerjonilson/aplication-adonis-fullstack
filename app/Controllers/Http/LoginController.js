94254ecf9ec02fce8a941ff7dbc3fc83e76507de
jonilsondeveloper <jonilsondeveloper@gmail.com>
2019-05-10 16:29:24 -0300

    pegar informacoes face

--- app/Controllers/Http/LoginController.js
+++ app/Controllers/Http/LoginController.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const User = use('App/Models/User')
+// const User = use('App/Models/User')
 
 class LoginController {
   async redirect ({ ally }) {
@@ -12,29 +12,41 @@ class LoginController {
       const fbUser = await ally.driver('facebook').getUser()
 
       // search for existing user
-      const whereClause = {
-        email: fbUser.getEmail()
-      }
+      // const whereClause = {
+      //   email: fbUser.getEmail()
+      // }
 
-      const userDb = await User.find(whereClause)
-      if (userDb) {
-        await auth.login(userDb)
-        return 'Logado'
-      }
+      // const userDb = await User.find(whereClause)
+      // if (userDb) {
+      //   await auth.login(userDb)
+      //   return 'Logado'
+      // }
 
       // user details to be saved
       const userDetails = {
         email: fbUser.getEmail(),
         token: fbUser.getAccessToken(),
-        login_source: 'facebook'
+        provider: 'facebook',
+        name: fbUser.getName(),
+        username: fbUser.getNickname(),
+        provider_id: fbUser.getId(),
+        avatar: fbUser.getAvatar()
       }
 
-      const user = await User.create(userDetails)
-      await auth.login(user)
-      return 'Logado'
+      return response.send(userDetails)
+
+      // const user = await User.create(userDetails)
+      // await auth.login(user)
+      // return 'Logado'
     } catch (error) {
       return 'Incapaz de autenticar. Tente mais tarde'
     }
+  }
+
+  async logout ({ auth, response }) {
+    await auth.logout()
+
+    response.redirect('/')
   }
 }
 
--- start/routes.js
+++ start/routes.js
@@ -18,7 +18,7 @@ const Route = use('Route')
 
 Route.on('/').render('welcome')
 Route.get('auth/facebook', 'LoginController.redirect').as('auth.social')
-Route.get('auth/url', 'LoginController.urlRedirect').as('auth.url')
 Route.get('authenticated/facebook', 'LoginController.callback').as(
   'auth.social.callback'
 )
+Route.get('logout', 'LoginController.logout').as('auth.logout')
