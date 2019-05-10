8e3d9f1975ed16579400d46e2deadfd1cfbbc6d7
jonilsondeveloper <jonilsondeveloper@gmail.com>
2019-05-10 15:14:12 -0300

    add logica login

--- app/Controllers/Http/LoginController.js
+++ app/Controllers/Http/LoginController.js
@@ -11,31 +11,27 @@ class LoginController {
     try {
       const fbUser = await ally.driver('facebook').getUser()
 
-      // user details to be saved
-      // const userDetails = {
-      //   email: fbUser.getEmail(),
-      //   token: fbUser.getAccessToken(),
-      //   login_source: 'facebook'
-      // }
-
       // search for existing user
       const whereClause = {
         email: fbUser.getEmail()
       }
 
-      const user = await User.find(whereClause)
-      // const user = await User.findOrCreate(
-      //   { email: whereClause },
-      //   { username: 'virk', email: 'virk@adonisjs.com' }
-      // )
+      const userDb = await User.find(whereClause)
+      if (userDb) {
+        await auth.login(userDb)
+        return 'Logado'
+      }
 
-      // const user = await User.findOrCreate(whereClause, userDetails)
-      // await auth.login(user)
+      // user details to be saved
+      const userDetails = {
+        email: fbUser.getEmail(),
+        token: fbUser.getAccessToken(),
+        login_source: 'facebook'
+      }
 
-      // return 'Logado'
-      console.log(user)
-      return user
-      // return response.send(user)
+      const user = await User.create(userDetails)
+      await auth.login(user)
+      return 'Logado'
     } catch (error) {
       return 'Incapaz de autenticar. Tente mais tarde'
     }
